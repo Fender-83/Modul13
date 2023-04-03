eba3019ec4cf62dee352b31ff134328af00ffdc1
Name <fenderpugach@gmail.com>
2023-04-03 16:10:44 +0700

    1 задача

---
+++ 13.6.1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+
+namespace _13._6._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var time = Stopwatch.StartNew(); 
+            
+            LinkedList<string> listLinked = new LinkedList<string>();
+            listLinked.AddFirst(File.ReadAllText("C:/Users/Fender/Desktop/Text1.txt"));
+            Console.WriteLine($"Производительность с использованием LinkedList<T> составила: {time.Elapsed.TotalMilliseconds}  мс.");
+            Console.WriteLine();
+
+            List<string> list = new List<string>();
+            list = File.ReadLines("C:/Users/Fender/Desktop/Text1.txt").ToList();
+            Console.WriteLine($"Производительность с использованием List<T> составила:{ time.Elapsed.TotalMilliseconds}  мс.");
+
+
+        }
+    }
+}
